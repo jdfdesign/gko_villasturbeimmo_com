d72afb7d52e27530dfe884f1aa04c30dc411f56f
regis bruggheman <regis@joufdesign.com>
2013-05-11 10:05:49 +0200

    bump

--- app/assets/javascripts/public.js
+++ app/assets/javascripts/public.js
@@ -1,6 +1,11 @@
 //= require jquery
 //= require jquery_ujs
-//= require twitter.bootstrap.2.2.1/bootstrap
+//= require twitter.bootstrap.2.2.1/bootstrap/transition
+//= require twitter.bootstrap.2.2.1/bootstrap/alert
+//= require twitter.bootstrap.2.2.1/bootstrap/button
+//= require twitter.bootstrap.2.2.1/bootstrap/collapse
+//= require twitter.bootstrap.2.2.1/bootstrap/dropdown
+//= require twitter.bootstrap.2.2.1/bootstrap/modal
 //= require jquery.throttledresize.js
 //= require jquery.mousewheel.3.0.6.js
 //= require jquery.jscrollpane.js
